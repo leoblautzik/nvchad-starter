7750022de24c6e2ce632c0ac86c236033080e886
leoblautzik <leoblautzik@gmail.com>
2025-09-30 15:34:38 -0300

    nvchad-leoblau-nas -> mappings actualizado

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,18 @@ map("i", "jk", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+map("n", "<leader><leader>", "<cmd> Telescope buffers <cr>", { desc = "Find Buffers" })
+
+-- Mover líneas o bloques con Alt+j / Alt+k
+
+-- Normal mode: mover línea actual
+map("n", "<leader>j", ":m .+1<CR>==", { silent = true, desc = "Mover línea abajo" })
+map("n", "<leader>k", ":m .-2<CR>==", { silent = true, desc = "Mover línea arriba" })
+
+-- Visual mode: mover bloque seleccionado
+map("v", "<leader>j", ":m '>+1<CR>gv=gv", { silent = true, desc = "Mover bloque abajo" })
+map("v", "<leader>k", ":m '<-2<CR>gv=gv", { silent = true, desc = "Mover bloque arriba" })
+
+-- Insert mode: mover línea actual sin salir de insert
+map("i", "<leader>j", "<Esc>:m .+1<CR>==gi", { silent = true, desc = "Mover línea abajo" })
+map("i", "<leader>k", "<Esc>:m .-2<CR>==gi", { silent = true, desc = "Mover línea arriba" })
